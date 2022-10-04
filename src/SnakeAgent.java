009093b5d4ebc26b01e2c2e1c05e6cd16bfc8799
orwellian <orwellian225@gmail.com>
2022-10-04 21:33:39 +0200

    Add time execution logging Add tupleSums function Add NullPointerException

--- src/SnakeAgent.java
+++ src/SnakeAgent.java
@@ -1,14 +1,15 @@
 import za.ac.wits.snake.DevelopmentAgent;
 
 import java.util.*;
+import java.io.*;
 
 public class SnakeAgent extends DevelopmentAgent {
 
     //Scaling coefficients for cost evaluation
-    final private float mew_snake = 0;
-    final private float mew_zombie = 0;
-    final private float mew_barrier = 0;
-    final private float mew_apple = 1;
+    final private double mew_snake = 1;
+    final private double mew_zombie = 0;
+    final private double mew_barrier = 0;
+    final private double mew_apple = 1;
 
     private Tuple[] snakes;
     private ArrayList<Tuple> barriers = new ArrayList<>();
@@ -48,14 +49,13 @@ public class SnakeAgent extends DevelopmentAgent {
         }
 
         while(true) {
+            long start_time = System.nanoTime();
             //Parsing Input ========================================================================================
             barriers.clear();
             String[] input = new String[12];
 
                 for (int i = 0; i < 12; i++) {
                     input[i] = in.nextLine();
-
-                    if(input[i].contains("Game Over")) { writer.close(); break; }
                 }
 
             apple.update(input[0].replace(" ", ","));
@@ -88,8 +88,12 @@ public class SnakeAgent extends DevelopmentAgent {
             //======================================================================================================
 
             //A* ===================================================================================================
-            //Tuple test = new Tuple(apple.x - 1, apple.y-2);
-            Tuple next = a_star(snake_me);
+            Tuple next;
+            try {
+                next = a_star(snake_me);
+            } catch(NullPointerException npe) {
+                next = null;
+            }
 
             if(next == null) { moveStraight(); }
             else if(next.x < snake_me.x) { moveWest(); }
@@ -98,8 +102,26 @@ public class SnakeAgent extends DevelopmentAgent {
             else if(next.y > snake_me.y) { moveSouth(); }
             //======================================================================================================
 
-            //printBoard();
+            long end_time = System.nanoTime();
 
+            double duration = Math.floor((end_time - start_time) / 1000000);
+
+            String time_output = "";
+            time_output += "Time: " + duration + "ms";
+
+            if(duration > 50) {
+                time_output += " Me: " + snake_me.toString();
+                time_output += " Apple: " + apple.toString();
+            }
+
+            try(FileWriter fw = new FileWriter("time.txt", true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter out = new PrintWriter(bw))
+            {
+                out.println(time_output);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
     }
@@ -110,33 +132,9 @@ public class SnakeAgent extends DevelopmentAgent {
     private void moveWest() { System.out.println(2); }
 
     private void moveStraight() { System.out.println(5); }
-    //private void moveLeft() { System.out.println(4); }
-    //private void moveRight() { System.out.println(6); }
 
     private double step_distance(Tuple p1, Tuple p2) { return Math.ceil(Math.sqrt( (p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y) )); }
-    private double h_cost(Tuple point) {
-        double val = 0;
-        val += mew_apple * step_distance(point, apple);
-        val += mew_snake * array_distance_sum(snakes, point);
-        val += mew_zombie * array_distance_sum(zombies, point);
-        val += mew_barrier * array_distance_sum(barriers, point);
-        return val;
-    }
 
-    private double array_distance_sum(Tuple[] points, Tuple point) {
-        double val = 0;
-        for (Tuple arr_point : points) {
-            val += step_distance(point, arr_point);
-        }
-        return val;
-    }
-    private double array_distance_sum(ArrayList<Tuple> points, Tuple point) {
-        double val = 0;
-        for (Tuple arr_point : points) {
-            val += step_distance(point, arr_point);
-        }
-        return val;
-    }
     private void mark_barriers(Tuple p1, Tuple p2) {
         if(p1.x > p2.x) {
 
@@ -165,6 +163,12 @@ public class SnakeAgent extends DevelopmentAgent {
         }
     }
 
+    private double tupleSums(Tuple[] points, Tuple origin) {
+        double sum = 0;
+        for(Tuple p: points) { sum += step_distance(p, origin); }
+        return sum;
+    }
+
     private Tuple a_star(Tuple start) {
         PriorityQueue<Vertex> open_list = new PriorityQueue<>((o1, o2) -> {
             if(o1.f < o2.f) {
@@ -190,7 +194,6 @@ public class SnakeAgent extends DevelopmentAgent {
         open_list.add(new Vertex(start, 0, 0));
 
         while(!open_list.isEmpty()) {
-            //logQueue(open_list);
             Vertex current = open_list.poll();
             ArrayList<Vertex> neighbours = new ArrayList<>();
 
@@ -214,12 +217,13 @@ public class SnakeAgent extends DevelopmentAgent {
                                 neighbour_point.y < 0 ||
                                 neighbour_point.y >= board_height ||
                                 barriers.contains(neighbour_point) ||
-                                closed_list.contains(new Vertex(neighbour_point, 0, 0))
+                                closed_list.indexOf(new Vertex(neighbour_point, 0, 0)) >= 0
                 ) {
                     continue;
                 }
 
-                double neighbour_h = h_cost(neighbour_point);
+                double neighbour_h = mew_apple * step_distance(neighbour_point, apple);
+
                 double neighbour_g = current.g + 1;
                 neighbours.add(new Vertex(neighbour_point, neighbour_h, neighbour_g));
 
@@ -231,14 +235,11 @@ public class SnakeAgent extends DevelopmentAgent {
             for(Vertex neighbour : neighbours) {
                 if(neighbour.point.equals(apple)) { found_apple = true; break; }
 
-                Vertex[] open_vertices = new Vertex[open_list.size()];
-                open_list.toArray(open_vertices);
-
                 boolean ov_found = false;
                 boolean cv_found = false;
-                for(Vertex ov: open_vertices) { if(ov.equals(neighbour) && ov.f < neighbour.f) { ov_found = true; } }
+                for(Vertex ov: open_list) { if(ov.equals(neighbour) && ov.f < neighbour.f) { ov_found = true; break; } }
                 if(ov_found) { continue; }
-                for(Vertex cv: closed_list) { if(cv.equals(neighbour) && cv.f < neighbour.f) { cv_found = true; } }
+                for(Vertex cv: closed_list) { if(cv.equals(neighbour) && cv.f < neighbour.f) { cv_found = true; break; } }
                 if(cv_found) { continue; }
 
                 open_list.add(neighbour);
