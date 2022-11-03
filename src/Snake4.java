a74c24a18ae5dd97af4e2022fa50118e37e4c375
Orwellian <orwellian225@gmail.com>
2022-11-03 12:54:43 +0200

    semi-cleaned up snake code

--- src/Snake4.java
+++ src/Snake4.java
@@ -5,18 +5,6 @@ import java.util.*;
 
 public class Snake4 extends DevelopmentAgent {
 
-    /*
-    private static class ga_params {
-        public static double µ_hx = 10.0; //heuristic multiplier
-        public static double µ_sm = 4.0; //snake midpoint
-        public static double µ_sx = 100.0; //snake multiplier
-        public static double µ_zm = 2.0; //zombie midpoint
-        public static double µ_zx = 100.0; //zombie multiplier
-        public static double µ_bm = 1.0; //barrier midpoint
-        public static double µ_bx = 50.0; //barrier multiplier
-    }
-    */
-
     private char[][] board;
     private boolean[][] invalid_walls;
     private ArrayList<Tuple> b;
@@ -51,38 +39,13 @@ public class Snake4 extends DevelopmentAgent {
         if(args.length > 1) {
         	agent.ga_file = args[1];
         	args = Arrays.copyOfRange(args, 0, 1);
-        };
+        }
 
         Snake4.start(agent, args);
     }
 
     @Override
     public void run() {
-        /*
-        try {
-            BufferedReader ga_input = new BufferedReader(new FileReader(ga_file));
-            ga_params.µ_hx = Double.parseDouble(ga_input.readLine().split(": ")[1]);
-            ga_params.µ_sm = Double.parseDouble(ga_input.readLine().split(": ")[1]);
-            ga_params.µ_sx = Double.parseDouble(ga_input.readLine().split(": ")[1]);
-            ga_params.µ_zm = Double.parseDouble(ga_input.readLine().split(": ")[1]);
-            ga_params.µ_zx = Double.parseDouble(ga_input.readLine().split(": ")[1]);
-            ga_params.µ_bm = Double.parseDouble(ga_input.readLine().split(": ")[1]);
-            ga_params.µ_bx = Double.parseDouble(ga_input.readLine().split(": ")[1]);
-            ga_input.close();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } catch (NullPointerException npe) {
-            ga_params.µ_hx = 1.0; //heuristic distance greater
-            ga_params.µ_sm = 4.0; //snake midpoint
-            ga_params.µ_sx = 0.0; //snake multiplier
-            ga_params.µ_zm = 4.0; //Zombie Midpoint
-            ga_params.µ_zx = 0.0; //zombie multiplier
-            ga_params.µ_bm = 1.0; //barrier midpoint
-            ga_params.µ_bx = 0.0; //barrier multiplier
-
-            //npe.printStackTrace();
-        }
-        */
 
         try {
             BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -96,25 +59,18 @@ public class Snake4 extends DevelopmentAgent {
 
             board = new char[game_config.width][game_config.height];
 
-            int frame_count = 0;
-            double ave_score = 0;
-            double ave_time = 0;
             while (true) {
-                ++frame_count;
                 //Parse
                 ArrayList<Tuple> s = new ArrayList<>(game_config.num_s);
-                ArrayList<Integer> s_lengths = new ArrayList<>(game_config.num_s);
                 ArrayList<Tuple> z = new ArrayList<>(game_config.num_z);
                 b = new ArrayList<>(game_config.num_s * 5 + game_config.num_z * 6);
                 invalid_walls = new boolean[game_config.width][game_config.height];
                 board = new char[game_config.width][game_config.height];
 
                 String apple_in = input.readLine();
-                //debug.log(apple_in);
                 
                 if(apple_in.contains("Game Over")) { break; }
-                
-                double frame_time_start = System.nanoTime();
+
                 apple_in = apple_in.replace(' ', ',');
                 apple = new Tuple(apple_in);
 
@@ -156,7 +112,6 @@ public class Snake4 extends DevelopmentAgent {
                     }
 
                     s.add(new Tuple(s_tokens[3]));
-                    s_lengths.add(Integer.parseInt(s_tokens[1]));
                     mark_barriers(Arrays.copyOfRange(s_tokens, 3, s_tokens.length));
                     b.remove(s.get(s.size() - 1));
                     maps.update(board, s.get(s.size() - 1), game_chars.snake);
@@ -174,7 +129,12 @@ public class Snake4 extends DevelopmentAgent {
                         next == null ||
                         array_can_reach(s, apple, 15) && !closest_snake(s, my_snake, apple)
                 ) {
-                    next = sector_move(apple);
+                    ArrayList<Tuple> game_objects = new ArrayList<>();
+                    game_objects.addAll(s);
+                    game_objects.addAll(z);
+                    game_objects.addAll(b);
+
+                    next = sector_move(apple, game_objects);
                 }
 
                 if (
@@ -186,21 +146,6 @@ public class Snake4 extends DevelopmentAgent {
 
 
                 System.out.println(move_direction(next));
-
-                double frame_t = (System.nanoTime() - frame_time_start) / 1e6;
-                //System.out.println("log " + frame_t);
-                ave_time += frame_t;
-                ave_score += my_length;
-
-                /*
-                if(frame_count == 2390) {
-                        BufferedWriter ga_output = new BufferedWriter(new FileWriter(ga_file));
-                        ga_output.write("Time: " + ave_time / frame_count + "\n");
-                        ga_output.write("Score: " + ave_score / frame_count + "\n");
-                        ga_output.write("Longest: " + this.getLongest() + "\n");
-                        ga_output.close();
-                }
-                */
 
             }
 
@@ -225,7 +170,56 @@ public class Snake4 extends DevelopmentAgent {
         return null;
     }
 
-    public Tuple sector_move(Tuple prev_goal) {
+    public Tuple sector_move(Tuple prev_goal, ArrayList<Tuple> objects) {
+        /*
+        class sector {
+            final int idx;
+            int density;
+
+            sector(int idx) {
+                this.idx = idx;
+            }
+
+            static int convert_tuple(Tuple t, Tuple size) {
+                //noinspection IntegerDivisionInFloatingPointContext
+                return (int)Math.floor( t.x/size.x ) + (int)Math.floor( t.y/size.y );
+            }
+
+            static Tuple convert_idx(int idx, int number, Tuple size) {
+                //Returns the midpoint of the sector
+                return new Tuple((idx+1) / number * size.x, idx % number * size.y);
+            }
+        }
+
+        int sector_count = 4;
+        Tuple sector_size = new Tuple(game_config.width / (sector_count/2) , game_config.height / (sector_count/2) );
+
+        Tuple test_md_0 = sector.convert_idx(0, sector_count, sector_size);
+        Tuple test_md_1 = sector.convert_idx(1, sector_count, sector_size);
+        Tuple test_md_2 = sector.convert_idx(2, sector_count, sector_size);
+        Tuple test_md_3 = sector.convert_idx(3, sector_count, sector_size);
+        System.out.println("log ");
+
+
+        PriorityQueue<sector> sector_pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.density));
+        ArrayList<sector> sector_list = new ArrayList<>(sector_count);
+
+        for(int i = 0; i < sector_count; i++) {
+            sector_list.add(new sector(i));
+        }
+
+        for(Tuple obj: objects) {
+            ++sector_list.get(sector.convert_tuple(obj, sector_size)).density;
+        }
+
+        sector_list.remove(sector.convert_tuple(prev_goal, sector_size)); //Remove the sector containing the goal
+
+        sector_pq.addAll(sector_list);
+
+        sector best_sector = sector_pq.poll();
+        assert best_sector != null;
+        Tuple sector_goal = sector.convert_idx(best_sector.idx, sector_count, sector_size);
+*/
         Tuple sector_goal = new Tuple(0,0);
 
         Tuple midpoint = new Tuple(game_config.width / 2, game_config.height / 2);
@@ -238,11 +232,12 @@ public class Snake4 extends DevelopmentAgent {
             while(invalid_point(sector_goal)) {
                 sector_goal.x -= 1;
 
-                if(sector_goal.x == midpoint.x) {
+                if (sector_goal.x == midpoint.x) {
                     sector_goal.y -= 1;
                     sector_goal.x = game_config.width - 1;
                 }
             }
+
 
         } else if(prev_goal.x < game_config.width && prev_goal.y < midpoint.y) {
             //Sector II -> Sector III
@@ -287,6 +282,8 @@ public class Snake4 extends DevelopmentAgent {
             return null;
         }
 
+
+
         for(int x = 0; x < game_config.width; x++) {
             for(int y = 0; y < game_config.height; y++) {
                 Tuple t = new Tuple(x, y);
@@ -298,7 +295,6 @@ public class Snake4 extends DevelopmentAgent {
         }
 
         maps.update(board, my_snake, game_chars.me);
-
         return tree_trace(a_star(my_snake, sector_goal), my_snake, sector_goal);
     }
 
@@ -492,28 +488,4 @@ public class Snake4 extends DevelopmentAgent {
         return true;
     }
 
-    public double sigmoid(double x, double midpoint, double multiplier) {
-        double scale = Math.sqrt(midpoint);
-        double e = Math.exp(1/scale * x - scale);
-        double f = -1 * e/(1 + e) + 1;
-        return multiplier * f;
-    }
-
-    private static class debug {
-        public static void log(String message) {
-            System.out.println("log " + message);
-        }
-
-        public static String print(char[][] map) {
-            StringBuilder result = new StringBuilder();
-            for(char[] row: map) {
-                for(char col: row) {
-                    result.append(col);
-                }
-                result.append("\n");
-            }
-
-            return result.toString();
-        }
-    }
 }
